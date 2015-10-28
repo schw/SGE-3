d0bcdaf68caab3f1e1933190feaeafcc7386e43a
Fabricio Lima <fabriciolima31@gmail.com>
2015-10-28 11:51:53 -0400

    removido

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sge',
-    'username' => 'root',
-    'password' => 'Bleach.123',
-    'charset' => 'utf8',
-];
