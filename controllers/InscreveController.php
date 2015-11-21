9256503cc79001077f54c1b73bcd2edf4526b495
Thiago Leite Takahashi <thiagoleitexd@gmail.com>
2015-11-21 01:29:08 -0200

    iniciando o certificados

--- controllers/InscreveController.php
+++ controllers/InscreveController.php
@@ -17,6 +17,11 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
+use yii\helpers\Html;
+
+use kartik\mpdf\Pdf;
+use mPDF;
+
 /**
  * InscreveController implements the CRUD actions for Inscreve model.
  */
@@ -334,5 +339,113 @@ class InscreveController extends Controller
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
+
+
+    public function actionPdf() {
+ 
+    // get your HTML raw content without any layouts or scripts
+   
+    // setup kartik\mpdf\Pdf component
+
+
+
+
+            $pdf = new mPDF('utf-8', 'A4-L');
+
+
+            $pdf->WriteHTML('');
+
+//inicio
+
+            $pdf->SetFont("Helvetica",'B', 14);
+            $pdf->MultiCell(0,6,"PODER EXECUTIVO",0, 'C');
+            $pdf->MultiCell(0,6,("MINISTÉRIO DA EDUCAÇÃO"),0, 'C');
+            $pdf->MultiCell(0,6,("INSTITUTO DE COMPUTAÇÃO"),0, 'C');
+            //$pdf->MultiCell(0,5,("-----------------"),0, 'C');
+            $pdf->SetDrawColor(0,0,0);
+            $pdf->Line(10,42,290,42);
+            $pdf->Image('../web/img/logo-brasil.jpg', 10, 7, 32.32);
+            $pdf->Image('../web/img/ufam.jpg', 260, 7, 25.25);
+
+
+ 
+//fim
+            $pdf->Ln(20);
+            $pdf->SetFont('Arial','B',22);
+            $pdf->MultiCell(0,4,("Certificado"),0, 'C');
+
+
+            
+            //aqui
+$nome= 'Thiago';
+$Descricao= 'Programar como se nao houvesse amanha';
+$Sigla= 'PROG';
+$cargaHoraria= '20.000';
+$data_texto= '20 de novembro de 2015';
+$retornaAtualMes = "fevereiro";
+
+        $pdf->SetFont('Arial','',18);
+
+        $pdf->Ln(25);
+
+
+
+
+        $pdf->WriteHTML('       Certificamos que <b>'. $nome.'</b> participou do evento <b>"'. 
+                $Descricao.'" ('.$Sigla.')</b>, com carga horaria de <b>'.$cargaHoraria.
+                ' horas</b>, realizado no dia<b> '. $data_texto .'</b>, na cidade de Manaus - AM.');
+
+
+        //$pdf->MultiCell(0,10,utf8_decode($tag),0, 'J');
+    
+        $pdf->Ln(22);
+        
+        $current = date('Y/m/d');
+        $currentTime = strtotime($current);
+        
+        $pdf->Cell(0,5,('Manaus, '. date('d', $currentTime).' de '. $retornaAtualMes. ' de '. date('Y', $currentTime).'.      '),0,1, 'R');
+
+
+
+
+            $pdf->SetFont('Helvetica','I',8);
+            $pdf->Line(10,185,280,185);
+            $pdf->SetXY(10, 180);
+            $pdf->MultiCell(0,5,"",0, 'C');
+            $pdf->MultiCell(0,4,("Av. Rodrigo Otávio, 6.200 - Campus Universitário Senador Arthur Virgílio Filho - CEP 69077-000 - Manaus, AM, Brasil"),0, 'C');
+            $pdf->MultiCell(0,4,(" Tel. (092) 3305-1193/2808/2809         E-mail: secretaria@icomp.ufam.edu.br          www.icomp.ufam.edu.br"),0, 'C');
+            //$pdf->Image('components/com_portalsecretaria/images/icon_telefone.jpg', '40', '290');
+            //$pdf->Image('components/com_portalsecretaria/images/icon_email.jpg', '73', '290');
+            //$pdf->Image('components/com_portalsecretaria/images/icon_casa.jpg', '134', '290');
+
+
+
+
+
+
+
+
+            // fim do aqui
+
+
+
+
+
+            $pdf->Output();
+
+            exit;
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 
 }
